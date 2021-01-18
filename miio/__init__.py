ba4091309db608a3c837c47d731b81866757fb80
Johnnie Ho <johnnieho89@gmail.com>
2021-01-18 13:03:08 +0100

    Use local library imports

--- miio/__init__.py
+++ miio/__init__.py
@@ -6,51 +6,51 @@ except ImportError:
     # python 3.8 and later
     from importlib.metadata import version  # type: ignore
 
-from miio.airconditioner_miot import AirConditionerMiot
-from miio.airconditioningcompanion import (
+from .airconditioner_miot import AirConditionerMiot
+from .airconditioningcompanion import (
     AirConditioningCompanion,
     AirConditioningCompanionV3,
 )
-from miio.airconditioningcompanionMCN import AirConditioningCompanionMcn02
-from miio.airdehumidifier import AirDehumidifier
-from miio.airfresh import AirFresh, AirFreshVA4
-from miio.airfresh_t2017 import AirFreshA1, AirFreshT2017
-from miio.airhumidifier import AirHumidifier, AirHumidifierCA1, AirHumidifierCB1
-from miio.airhumidifier_jsq import AirHumidifierJsq
-from miio.airhumidifier_miot import AirHumidifierMiot
-from miio.airhumidifier_mjjsq import AirHumidifierMjjsq
-from miio.airpurifier import AirPurifier
-from miio.airpurifier_airdog import AirDogX3, AirDogX5, AirDogX7SM
-from miio.airpurifier_miot import AirPurifierMiot
-from miio.airqualitymonitor import AirQualityMonitor
-from miio.aqaracamera import AqaraCamera
-from miio.ceil import Ceil
-from miio.chuangmi_camera import ChuangmiCamera
-from miio.chuangmi_ir import ChuangmiIr
-from miio.chuangmi_plug import ChuangmiPlug, Plug, PlugV1, PlugV3
-from miio.cooker import Cooker
-from miio.curtain_youpin import CurtainMiot
-from miio.device import Device
-from miio.exceptions import DeviceError, DeviceException
-from miio.fan import Fan, FanP5, FanSA1, FanV2, FanZA1, FanZA4
-from miio.fan_leshow import FanLeshow
-from miio.fan_miot import FanMiot, FanP9, FanP10, FanP11
-from miio.gateway import Gateway
-from miio.heater import Heater
-from miio.heater_miot import HeaterMiot
-from miio.huizuo import Huizuo
-from miio.miot_device import MiotDevice
-from miio.philips_bulb import PhilipsBulb, PhilipsWhiteBulb
-from miio.philips_eyecare import PhilipsEyecare
-from miio.philips_moonlight import PhilipsMoonlight
-from miio.philips_rwread import PhilipsRwread
-from miio.powerstrip import PowerStrip
-from miio.protocol import Message, Utils
-from miio.pwzn_relay import PwznRelay
-from miio.toiletlid import Toiletlid
-from miio.vacuum import Vacuum, VacuumException
-from miio.vacuum_tui import VacuumTUI
-from miio.vacuumcontainers import (
+from .airconditioningcompanionMCN import AirConditioningCompanionMcn02
+from .airdehumidifier import AirDehumidifier
+from .airfresh import AirFresh, AirFreshVA4
+from .airfresh_t2017 import AirFreshA1, AirFreshT2017
+from .airhumidifier import AirHumidifier, AirHumidifierCA1, AirHumidifierCB1
+from .airhumidifier_jsq import AirHumidifierJsq
+from .airhumidifier_miot import AirHumidifierMiot
+from .airhumidifier_mjjsq import AirHumidifierMjjsq
+from .airpurifier import AirPurifier
+from .airpurifier_airdog import AirDogX3, AirDogX5, AirDogX7SM
+from .airpurifier_miot import AirPurifierMiot
+from .airqualitymonitor import AirQualityMonitor
+from .aqaracamera import AqaraCamera
+from .ceil import Ceil
+from .chuangmi_camera import ChuangmiCamera
+from .chuangmi_ir import ChuangmiIr
+from .chuangmi_plug import ChuangmiPlug, Plug, PlugV1, PlugV3
+from .cooker import Cooker
+from .curtain_youpin import CurtainMiot
+from .device import Device
+from .exceptions import DeviceError, DeviceException
+from .fan import Fan, FanP5, FanSA1, FanV2, FanZA1, FanZA4
+from .fan_leshow import FanLeshow
+from .fan_miot import FanMiot, FanP9, FanP10, FanP11
+from .gateway import Gateway
+from .heater import Heater
+from .heater_miot import HeaterMiot
+from .huizuo import Huizuo
+from .miot_device import MiotDevice
+from .philips_bulb import PhilipsBulb, PhilipsWhiteBulb
+from .philips_eyecare import PhilipsEyecare
+from .philips_moonlight import PhilipsMoonlight
+from .philips_rwread import PhilipsRwread
+from .powerstrip import PowerStrip
+from .protocol import Message, Utils
+from .pwzn_relay import PwznRelay
+from .toiletlid import Toiletlid
+from .vacuum import Vacuum, VacuumException
+from .vacuum_tui import VacuumTUI
+from .vacuumcontainers import (
     CleaningDetails,
     CleaningSummary,
     ConsumableStatus,
@@ -58,14 +58,14 @@ from miio.vacuumcontainers import (
     Timer,
     VacuumStatus,
 )
-from miio.viomivacuum import ViomiVacuum
-from miio.waterpurifier import WaterPurifier
-from miio.waterpurifier_yunmi import WaterPurifierYunmi
-from miio.wifirepeater import WifiRepeater
-from miio.wifispeaker import WifiSpeaker
-from miio.yeelight import Yeelight
-from miio.yeelight_dual_switch import YeelightDualControlModule
+from .viomivacuum import ViomiVacuum
+from .waterpurifier import WaterPurifier
+from .waterpurifier_yunmi import WaterPurifierYunmi
+from .wifirepeater import WifiRepeater
+from .wifispeaker import WifiSpeaker
+from .yeelight import Yeelight
+from .yeelight_dual_switch import YeelightDualControlModule
 
-from miio.discovery import Discovery
+from .discovery import Discovery
 
 __version__ = version("python-miio")
